98413635dba69d1667253a70bafb6cf30d95de7f
YE GAO <sortsimilar@gmail.com>
2023-10-21 12:47:12 +0800

    Update timer.hpp

--- example/timer.hpp
+++ example/timer.hpp
@@ -3,25 +3,28 @@
 #include <chrono>
 #include <iostream>
 
-class Timer {
- public:
+class Timer
+{
+public:
   Timer()
       : start_(std::chrono::high_resolution_clock::now()),
-        end_(std::chrono::high_resolution_clock::now()) {}
+        end_(std::chrono::high_resolution_clock::now())
+        {}
 
-  void reset() { start_ = std::chrono::high_resolution_clock::now(); }
+    void reset() { start_ = std::chrono::high_resolution_clock::now(); }
 
-  void stop() { end_ = std::chrono::high_resolution_clock::now(); }
+    void stop() { end_ = std::chrono::high_resolution_clock::now(); }
 
-  double elapsedSeconds() const {
-    auto timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(
-        end_ - start_);
-    return timeSpan.count();
-  }
+    double elapsedSeconds() const
+    {
+        auto timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(
+            end_ - start_);
+        return timeSpan.count();
+    }
 
- private:
-  std::chrono::high_resolution_clock::time_point start_;
-  std::chrono::high_resolution_clock::time_point end_;
+private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::chrono::high_resolution_clock::time_point end_;
 };
 
 class ScopedTimer : public Timer {
