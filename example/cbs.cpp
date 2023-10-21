dc7f688c2494fae441706e4b4ce93cb2d2c4bde8
YE GAO <sortsimilar@gmail.com>
2023-10-21 08:52:32 +0800

    Update cbs.cpp

--- example/cbs.cpp
+++ example/cbs.cpp
@@ -160,31 +160,32 @@ namespace std
     };
 }
 
-struct EdgeConstraint {
-  EdgeConstraint(int time, int x1, int y1, int x2, int y2)
+struct EdgeConstraint
+{
+    EdgeConstraint(int time, int x1, int y1, int x2, int y2)
       : time(time), x1(x1), y1(y1), x2(x2), y2(y2) {}
-  int time;
-  int x1;
-  int y1;
-  int x2;
-  int y2;
-
-  bool operator<(const EdgeConstraint& other) const
-  {
+    int time;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    
+    bool operator<(const EdgeConstraint& other) const
+    {
     return tie(time, x1, y1, x2, y2) <
            tie(other.time, other.x1, other.y1, other.x2, other.y2);
-  }
+    }
 
-  bool operator==(const EdgeConstraint& other) const
-  {
+    bool operator==(const EdgeConstraint& other) const
+    {
     return tie(time, x1, y1, x2, y2) ==
            tie(other.time, other.x1, other.y1, other.x2, other.y2);
-  }
+    }
 
-  friend ostream& operator<<(ostream& os, const EdgeConstraint& c) {
+    friend ostream& operator<<(ostream& os, const EdgeConstraint& c) {
     return os << "EC(" << c.time << "," << c.x1 << "," << c.y1 << "," << c.x2
               << "," << c.y2 << ")";
-  }
+    }
 };
 
 namespace std
