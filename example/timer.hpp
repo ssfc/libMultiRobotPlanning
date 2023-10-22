d773a228000944d9eee16f8aab14e3d899222007
YE GAO <sortsimilar@gmail.com>
2023-10-22 09:00:15 +0800

    Update timer.hpp

--- example/timer.hpp
+++ example/timer.hpp
@@ -6,29 +6,29 @@
 class Timer
 {
 private:
-    std::chrono::high_resolution_clock::time_point start_;
-    std::chrono::high_resolution_clock::time_point end_;
+    std::chrono::high_resolution_clock::time_point start_time;
+    std::chrono::high_resolution_clock::time_point end_time;
 
 public:
     Timer()
-    : start_(std::chrono::high_resolution_clock::now()),
-      end_(std::chrono::high_resolution_clock::now())
+    : start_time(std::chrono::high_resolution_clock::now()),
+      end_time(std::chrono::high_resolution_clock::now())
     {}
 
     void reset()
     {
-        start_ = std::chrono::high_resolution_clock::now();
+        start_time = std::chrono::high_resolution_clock::now();
     }
 
     void stop()
     {
-        end_ = std::chrono::high_resolution_clock::now();
+        end_time = std::chrono::high_resolution_clock::now();
     }
 
     double elapsedSeconds() const
     {
         auto timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(
-            end_ - start_);
+            end_time - start_time);
 
         return timeSpan.count();
     }
