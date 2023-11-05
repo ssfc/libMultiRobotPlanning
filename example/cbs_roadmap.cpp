ad3898861fcba101e2d412a14c3f91742ae682a6
YE GAO <sortsimilar@gmail.com>
2023-11-05 14:23:41 +0800

    Update cbs_roadmap.cpp

--- example/cbs_roadmap.cpp
+++ example/cbs_roadmap.cpp
@@ -270,134 +270,147 @@ private:
     int m_lowLevelExpanded;
     bool m_disappearAtGoal;
     int m_timeLimit;
-    
+
 public:
     Environment(const roadmap_t& roadmap,
               const std::vector<vertex_t>& goals, bool disappearAtGoal = false)
-      : m_roadmap(roadmap),
-        m_goals(std::move(goals)),
-        m_agentIdx(0),
-        m_constraints(nullptr),
-        m_lastGoalConstraint(-1),
-        m_highLevelExpanded(0),
-        m_lowLevelExpanded(0),
-        m_disappearAtGoal(disappearAtGoal)
+              : m_roadmap(roadmap),
+              m_goals(std::move(goals)),
+              m_agentIdx(0),
+              m_constraints(nullptr),
+              m_lastGoalConstraint(-1),
+              m_highLevelExpanded(0),
+              m_lowLevelExpanded(0),
+              m_disappearAtGoal(disappearAtGoal)
     {
-    auto V = boost::num_vertices(m_roadmap);
-    // Upper bound on the makespan, see
-    // Jingjin Yu, Daniela Rus:
-    // Pebble Motion on Graphs with Rotations: Efficient Feasibility Tests and Planning Algorithms. WAFR 2014: 729-746
-    // NOTE: that the constant factor is not known
-    m_timeLimit = pow(V, 3);
+        auto V = boost::num_vertices(m_roadmap);
+        // Upper bound on the makespan, see
+        // Jingjin Yu, Daniela Rus:
+        // Pebble Motion on Graphs with Rotations: Efficient Feasibility Tests and Planning Algorithms. WAFR 2014: 729-746
+        // NOTE: that the constant factor is not known
+        m_timeLimit = pow(V, 3);
     }
 
     Environment(const Environment&) = delete;
     Environment& operator=(const Environment&) = delete;
 
-    void setLowLevelContext(size_t agentIdx, const Constraints* constraints) {
-    assert(constraints);  // NOLINT
-    m_agentIdx = agentIdx;
-    m_constraints = constraints;
-    m_lastGoalConstraint = -1;
-    for (const auto& vc : constraints->vertexConstraints) {
-      if (vc.vertex == m_goals[m_agentIdx]) {
-        m_lastGoalConstraint = std::max(m_lastGoalConstraint, vc.time);
-      }
-    }
-    }
-
-    int admissible_heuristic(const State& s) {
-    // TODO: we should actually compute this using A*
-    return 0;
-    }
-
-    bool is_solution(const State& s) {
-    return s.vertex == m_goals[m_agentIdx] &&
-           s.time > m_lastGoalConstraint;
-    }
-
-    void get_neighbors(const State& s,
-                    std::vector<Neighbor<State, Action, int> >& neighbors) {
-    // std::cout << "#VC " << constraints.vertexConstraints.size() << std::endl;
-    // for(const auto& vc : constraints.vertexConstraints) {
-    //   std::cout << "  " << vc.time << "," << vc.x << "," << vc.y <<
-    //   std::endl;
-    // }
-    neighbors.clear();
-
-    if (s.time > m_timeLimit) {
-      return;
-    }
-
-    auto es = boost::out_edges(s.vertex, m_roadmap);
-    for (auto eit = es.first; eit != es.second; ++eit) {
-      vertex_t v = boost::target(*eit, m_roadmap);
-      State n(s.time + 1, v);
-      if (location_valid(n) && transitionValid(s.time, *eit)) {
-        neighbors.emplace_back(
-            Neighbor<State, Action, int>(n, *eit, 1));
-      }
-    }
-
-    // // Wait action
-    // {
-    //   State n(s.time + 1, s.vertex);
-    //   if (location_valid(n)) {
-    //     neighbors.emplace_back(
-    //         Neighbor<State, Action, int>(n, edge_t(), 1));
-    //   }
-    // }
-    }
-
-    bool getFirstConflict(
-      const std::vector<PlanResult<State, Action, int> >& solution,
-      Conflict& result) {
-    size_t max_t = 0;
-    for (const auto& sol : solution) {
-      max_t = std::max<size_t>(max_t, sol.path.size() - 1);
-    }
-
-    for (size_t t = 0; t <= max_t; ++t) {
-      // check vertex/vertex collisions
-      for (size_t i = 0; i < solution.size(); ++i) {
-        State state1 = getState(i, solution, t);
-        for (size_t j = i + 1; j < solution.size(); ++j) {
-          State state2 = getState(j, solution, t);
-          if (state1.equalExceptTime(state2)) {
-            result.time = t;
-            result.agent1 = i;
-            result.agent2 = j;
-            result.type = Conflict::Vertex;
-            result.vertex = state1.vertex;
-            return true;
-          }
+    void setLowLevelContext(size_t agentIdx, const Constraints* constraints)
+    {
+        assert(constraints);  // NOLINT
+        m_agentIdx = agentIdx;
+        m_constraints = constraints;
+        m_lastGoalConstraint = -1;
+        for (const auto& vc : constraints->vertexConstraints)
+        {
+            if (vc.vertex == m_goals[m_agentIdx])
+            {
+                m_lastGoalConstraint = std::max(m_lastGoalConstraint, vc.time);
+            }
         }
-      }
-      // edge/edge
-      for (size_t i = 0; i < solution.size(); ++i) {
-        for (size_t j = i + 1; j < solution.size(); ++j) {
-          if (t < solution[i].actions.size() &&
-              t < solution[j].actions.size()) {
-            auto e1 = solution[i].actions[t].first;
-            auto e2 = solution[j].actions[t].first;
-            if (e1.m_eproperty) {
-              const auto& ce = m_roadmap[e1].conflictingEdges;
-              if (ce.find(e2) != ce.end()) {
-                result.time = t;
-                result.agent1 = i;
-                result.agent2 = j;
-                result.type = Conflict::Edge;
-                result.edge1 = e1;
-                result.edge2 = e2;
-                return true;
+    }
+
+    int admissible_heuristic(const State& s)
+    {
+        // TODO: we should actually compute this using A*
+        return 0;
+    }
+
+    bool is_solution(const State& s)
+    {
+        return s.vertex == m_goals[m_agentIdx] &&
+               s.time > m_lastGoalConstraint;
+    }
+
+    void get_neighbors(const State& s, std::vector<Neighbor<State, Action, int> >& neighbors)
+    {
+        // std::cout << "#VC " << constraints.vertexConstraints.size() << std::endl;
+        // for(const auto& vc : constraints.vertexConstraints) {
+        //   std::cout << "  " << vc.time << "," << vc.x << "," << vc.y <<
+        //   std::endl;
+        // }
+        neighbors.clear();
+
+        if (s.time > m_timeLimit)
+        {
+            return;
+        }
+
+        auto es = boost::out_edges(s.vertex, m_roadmap);
+        for (auto eit = es.first; eit != es.second; ++eit)
+        {
+            vertex_t v = boost::target(*eit, m_roadmap);
+            State n(s.time + 1, v);
+            if (location_valid(n) && transitionValid(s.time, *eit))
+            {
+                neighbors.emplace_back(Neighbor<State, Action, int>(n, *eit, 1));
+            }
+        }
+
+        // // Wait action
+        // {
+        //   State n(s.time + 1, s.vertex);
+        //   if (location_valid(n)) {
+        //     neighbors.emplace_back(
+        //         Neighbor<State, Action, int>(n, edge_t(), 1));
+        //   }
+        // }
+    }
+
+    bool getFirstConflict(const std::vector<PlanResult<State, Action, int> >& solution, Conflict& result)
+    {
+        size_t max_t = 0;
+        for (const auto& sol : solution)
+        {
+            max_t = std::max<size_t>(max_t, sol.path.size() - 1);
+        }
+
+        for (size_t t = 0; t <= max_t; ++t)
+        {
+            // check vertex/vertex collisions
+            for (size_t i = 0; i < solution.size(); ++i)
+            {
+                State state1 = getState(i, solution, t);
+                for (size_t j = i + 1; j < solution.size(); ++j)
+                {
+                    State state2 = getState(j, solution, t);
+                    if (state1.equalExceptTime(state2))
+                    {
+                        result.time = t;
+                        result.agent1 = i;
+                        result.agent2 = j;
+                        result.type = Conflict::Vertex;
+                        result.vertex = state1.vertex;
+
+                        return true;
+                    }
+                }
+            }
+            
+            // edge/edge
+            for (size_t i = 0; i < solution.size(); ++i) {
+            for (size_t j = i + 1; j < solution.size(); ++j) {
+              if (t < solution[i].actions.size() &&
+                  t < solution[j].actions.size()) {
+                auto e1 = solution[i].actions[t].first;
+                auto e2 = solution[j].actions[t].first;
+                if (e1.m_eproperty) {
+                  const auto& ce = m_roadmap[e1].conflictingEdges;
+                  if (ce.find(e2) != ce.end()) {
+                    result.time = t;
+                    result.agent1 = i;
+                    result.agent2 = j;
+                    result.type = Conflict::Edge;
+                    result.edge1 = e1;
+                    result.edge2 = e2;
+                    return true;
+                  }
+                }
               }
             }
-          }
+            }
         }
-      }
-    }
 
-    return false;
+        return false;
     }
 
     void createConstraintsFromConflict(
