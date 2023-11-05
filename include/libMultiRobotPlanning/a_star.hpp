dbb7075857fc25badd574b7336eca0cea412bd92
YE GAO <sortsimilar@gmail.com>
2023-11-05 18:22:52 +0800

    458	Move Location definition to util.hpp.

--- include/libMultiRobotPlanning/a_star.hpp
+++ include/libMultiRobotPlanning/a_star.hpp
@@ -14,49 +14,6 @@
 using libMultiRobotPlanning::Neighbor;
 using libMultiRobotPlanning::PlanResult;
 
-class Location  // 模板类Location实例化
-{
-public:
-    int x;
-    int y;
-
-public:
-    Location() = default;
-    Location(int x, int y) : x(x), y(y) {}
-
-    bool operator==(const Location& other) const
-    {
-        return std::tie(x, y) == std::tie(other.x, other.y);
-    }
-
-    bool operator<(const Location& other) const
-    {
-        return std::tie(x, y) < std::tie(other.x, other.y);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Location& location)
-    {
-        return os << "(" << location.x << "," << location.y << ")";
-    }
-};
-
-// 为了使用Location进行map, 吾人的程序里也有。
-namespace std
-{
-    template <>
-    struct hash<Location>
-    {
-        size_t operator()(const Location& location) const
-        {
-            size_t seed = 0;
-            boost::hash_combine(seed, location.x);
-            boost::hash_combine(seed, location.y);
-
-            return seed;
-        }
-    };
-}
-
 /*!
   \example a_star.cpp Simple example using a 2D grid world and
   up/down/left/right
