920f3590984e66bbf5d868585e83486dc9e6a8bb
YE GAO <sortsimilar@gmail.com>
2023-11-06 19:17:06 +0800

    Update a_star_isolated.hpp

--- include/libMultiRobotPlanning/a_star_isolated.hpp
+++ include/libMultiRobotPlanning/a_star_isolated.hpp
@@ -107,6 +107,50 @@ public:
     void onDiscover(const Location& /*s*/, int /*fScore*/, int /*gScore*/) {}
 };
 
+class AStarNode
+{
+public:
+    Location location;
+    int f_score;
+    int g_score;
+
+    // 定义 handle: 就是上面那个HeapHandle
+    typename boost::heap::fibonacci_heap<AStarNode>::handle_type handle;
+    // typename boost::heap::d_ary_heap<AStarNode, boost::heap::arity<2>, boost::heap::mutable_<true>>::handle_type handle;
+
+public:
+    AStarNode(const Location& input_state, int input_fScore, int input_gScore)
+            : location(input_state),
+              f_score(input_fScore),
+              g_score(input_gScore)
+    {}
+
+    bool operator<(const AStarNode& other) const
+    {
+        // Sort order
+        // 1. lowest f_score
+        // 2. highest g_score
+
+        // Our heap is a maximum heap, so we invert the comperator function here
+        if (f_score != other.f_score)
+        {
+            return f_score > other.f_score;
+        }
+        else
+        {
+            return g_score < other.g_score;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const AStarNode& node)
+    {
+        os << "location: " << node.location << " f_score: " << node.f_score
+           << " g_score: " << node.g_score;
+
+        return os;
+    }
+};
+
 /*!
   \example a_star.cpp Simple example using a 2D grid world and
   up/down/left/right
@@ -152,50 +196,6 @@ class AStar
 {
 private:
     // inner class declaration.
-    class AStarNode
-    {
-    public:
-        Location location;
-        int f_score;
-        int g_score;
-
-        // 定义 handle: 就是上面那个HeapHandle
-        typename boost::heap::fibonacci_heap<AStarNode>::handle_type handle;
-        // typename boost::heap::d_ary_heap<AStarNode, boost::heap::arity<2>, boost::heap::mutable_<true>>::handle_type handle;
-
-    public:
-        AStarNode(const Location& input_state, int input_fScore, int input_gScore)
-                : location(input_state),
-                  f_score(input_fScore),
-                  g_score(input_gScore)
-        {}
-
-        bool operator<(const AStarNode& other) const
-        {
-            // Sort order
-            // 1. lowest f_score
-            // 2. highest g_score
-
-            // Our heap is a maximum heap, so we invert the comperator function here
-            if (f_score != other.f_score)
-            {
-                return f_score > other.f_score;
-            }
-            else
-            {
-                return g_score < other.g_score;
-            }
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const AStarNode& node)
-        {
-            os << "location: " << node.location << " f_score: " << node.f_score
-               << " g_score: " << node.g_score;
-
-            return os;
-        }
-    };
-
     // member vars
     Environment environment; // include map size, obstacle position, agent goal.
     // 定义openSet_t和fibHeapHandle_t
