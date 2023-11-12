c597316431eba46332b95e6dee38df1645f0eae5
YE GAO <sortsimilar@gmail.com>
2023-11-12 18:39:14 +0800

    update

---
+++ benchmark/read_filename.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+
+folder_path = r'C:\gitcloud\libMultiRobotPlanning\benchmark\8x8_obst12'
+file_list = os.listdir(folder_path)
+
+df = pd.DataFrame(file_list, columns=['File Names'])
+
+df.to_excel('runtime.xlsx', index=False)
