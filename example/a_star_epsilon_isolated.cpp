7070c0a51a695f2991599dae79c65356b3134e5e
YE GAO <sortsimilar@gmail.com>
2023-11-25 15:38:08 +0800

    430	Remove typename Action in AgentPlan.

--- example/a_star_epsilon_isolated.cpp
+++ example/a_star_epsilon_isolated.cpp
@@ -165,7 +165,7 @@ int main(int argc, char* argv[])
 
     AStarEpsilon<Location, Action, int, Environment> astar(env, w);
 
-    AgentPlan<Action, int> solution;
+    AgentPlan<int> solution;
 
     if (env.location_valid(start)) {
         is_success = astar.search(start, solution);
--- include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
+++ include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
@@ -46,7 +46,7 @@ struct Child
     {}
 };
 
-template <typename Action, typename Cost>
+template <typename Cost>
 struct AgentPlan
 {
     // path constructing locations and their g_score
@@ -126,7 +126,7 @@ purposes.
         AStarEpsilon(Environment& environment, float w)
                 : m_env(environment), m_w(w) {}
 
-        bool search(const State& startState, AgentPlan<Action, Cost>& solution)
+        bool search(const State& startState, AgentPlan<Cost>& solution)
         {
             solution.path.clear();
             solution.path.emplace_back(std::make_pair<>(startState, 0));
