0e8b988e7e8e4505fc0a72557013b9b27fd6d2f0
YE GAO <sortsimilar@gmail.com>
2023-11-25 22:32:56 +0800

    454	Replace typedef with using.

--- include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
+++ include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
@@ -298,8 +298,8 @@ private:
     // typedef typename boost::heap::d_ary_heap<AStarEpsilonNode, boost::heap::arity<2>,
     // boost::heap::mutable_<true> > openSet_t;
     // typedef typename openSet_t::handle_type fibHeapHandle_t;
-    typedef typename boost::heap::d_ary_heap<fibHeapHandle_t, boost::heap::arity<2>, boost::heap::mutable_<true>,
-        boost::heap::compare<compareFocalHeuristic> > focalSet_t;
+    using focalSet_t = typename boost::heap::d_ary_heap<fibHeapHandle_t, boost::heap::arity<2>, boost::heap::mutable_<true>,
+        boost::heap::compare<compareFocalHeuristic> >;
 
 public:
     AStarEpsilon(Environment& environment, float w)
