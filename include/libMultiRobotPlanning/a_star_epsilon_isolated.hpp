6b9841b69d7f02c97d3bc9741b152aa9811b0a18
YE GAO <sortsimilar@gmail.com>
2023-11-25 17:18:42 +0800

    445	Modify format.

--- include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
+++ include/libMultiRobotPlanning/a_star_epsilon_isolated.hpp
@@ -522,21 +522,19 @@ private:
         }
     };
 
-#ifdef USE_FIBONACCI_HEAP
+    #ifdef USE_FIBONACCI_HEAP
         // typedef typename boost::heap::fibonacci_heap<AStarEpsilonNode> openSet_t;
-  // typedef typename openSet_t::handle_type fibHeapHandle_t;
-  typedef typename boost::heap::fibonacci_heap<
-      fibHeapHandle_t, boost::heap::compare<compareFocalHeuristic> >
-      focalSet_t;
-#else
+        // typedef typename openSet_t::handle_type fibHeapHandle_t;
+        typedef typename boost::heap::fibonacci_heap<
+            fibHeapHandle_t, boost::heap::compare<compareFocalHeuristic> > focalSet_t;
+    #else
         // typedef typename boost::heap::d_ary_heap<AStarEpsilonNode, boost::heap::arity<2>,
         // boost::heap::mutable_<true> > openSet_t;
         // typedef typename openSet_t::handle_type fibHeapHandle_t;
         typedef typename boost::heap::d_ary_heap<
                 fibHeapHandle_t, boost::heap::arity<2>, boost::heap::mutable_<true>,
-        boost::heap::compare<compareFocalHeuristic> >
-        focalSet_t;
-#endif
+        boost::heap::compare<compareFocalHeuristic> > focalSet_t;
+    #endif
 
     private:
         Environment& m_env;
