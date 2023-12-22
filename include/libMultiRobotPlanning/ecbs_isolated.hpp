7757e77fa3d143e26bf14b1b01804f586a52a5ec
YE GAO <sortsimilar@gmail.com>
2023-12-22 18:08:49 +0800

    152	Rename Location to TimeLocation.

--- include/libMultiRobotPlanning/ecbs_isolated.hpp
+++ include/libMultiRobotPlanning/ecbs_isolated.hpp
@@ -19,17 +19,17 @@
 
 
 
-template <typename Location, typename Action, typename Cost>
+template <typename TimeLocation, typename Action, typename Cost>
 struct Neighbor
 {
     //! neighboring location
-    Location location;
+    TimeLocation location;
     //! action to get to the neighboring location
     Action action;
     //! cost to get to the neighboring location, usually 1
     Cost cost;
 
-    Neighbor(const Location& input_location, const Action& input_action, Cost input_cost)
+    Neighbor(const TimeLocation& input_location, const Action& input_action, Cost input_cost)
             : location(input_location),
               action(input_action),
               cost(input_cost)
