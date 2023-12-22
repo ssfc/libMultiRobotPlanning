97f24ed34148013e6c8cc2eabaaefb1022404fd5
YE GAO <sortsimilar@gmail.com>
2023-12-23 01:04:07 +0800

    174	Move struct Conflict.

--- example/ecbs_isolated.cpp
+++ example/ecbs_isolated.cpp
@@ -38,34 +38,6 @@ std::ostream& operator<<(std::ostream& os, const Action& a) {
 
 ///
 
-struct Conflict {
-    enum Type {
-        Vertex,
-        Edge,
-    };
-
-    int time;
-    size_t agent1;
-    size_t agent2;
-    Type type;
-
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-
-    friend std::ostream& operator<<(std::ostream& os, const Conflict& c) {
-        switch (c.type) {
-            case Vertex:
-                return os << c.time << ": Vertex(" << c.x1 << "," << c.y1 << ")";
-            case Edge:
-                return os << c.time << ": Edge(" << c.x1 << "," << c.y1 << "," << c.x2
-                          << "," << c.y2 << ")";
-        }
-        return os;
-    }
-};
-
 struct VertexConstraint {
     VertexConstraint(int time, int x, int y) : time(time), x(x), y(y) {}
     int time;
--- include/libMultiRobotPlanning/ecbs_isolated.hpp
+++ include/libMultiRobotPlanning/ecbs_isolated.hpp
@@ -293,6 +293,36 @@ private:
     float m_w;
 };
 
+
+struct Conflict {
+    enum Type {
+        Vertex,
+        Edge,
+    };
+
+    int time;
+    size_t agent1;
+    size_t agent2;
+    Type type;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    friend std::ostream& operator<<(std::ostream& os, const Conflict& c) {
+        switch (c.type) {
+            case Vertex:
+                return os << c.time << ": Vertex(" << c.x1 << "," << c.y1 << ")";
+            case Edge:
+                return os << c.time << ": Edge(" << c.x1 << "," << c.y1 << "," << c.x2
+                          << "," << c.y2 << ")";
+        }
+        return os;
+    }
+};
+
+
 template <typename Conflict, typename Constraints, typename Environment>
 class ECBS {
 public:
