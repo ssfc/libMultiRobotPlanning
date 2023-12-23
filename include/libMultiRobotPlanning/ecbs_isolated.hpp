99fb3b3842ea86c67cfb27f6b3c5b0d21469278b
YE GAO <sortsimilar@gmail.com>
2023-12-23 18:39:56 +0800

    format

--- include/libMultiRobotPlanning/ecbs_isolated.hpp
+++ include/libMultiRobotPlanning/ecbs_isolated.hpp
@@ -58,7 +58,8 @@ struct PlanResult
 class Conflict
 {
 public:
-    enum Type {
+    enum Type
+    {
         Vertex,
         Edge,
     };
@@ -98,23 +99,29 @@ public:
 public:
     VertexConstraint(int time, int x, int y) : time(time), x(x), y(y) {}
 
-    bool operator<(const VertexConstraint& other) const {
+    bool operator<(const VertexConstraint& other) const
+    {
         return std::tie(time, x, y) < std::tie(other.time, other.x, other.y);
     }
 
-    bool operator==(const VertexConstraint& other) const {
+    bool operator==(const VertexConstraint& other) const
+    {
         return std::tie(time, x, y) == std::tie(other.time, other.x, other.y);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const VertexConstraint& c) {
+    friend std::ostream& operator<<(std::ostream& os, const VertexConstraint& c)
+    {
         return os << "VC(" << c.time << "," << c.x << "," << c.y << ")";
     }
 };
 
-namespace std {
+namespace std
+{
     template <>
-    struct hash<VertexConstraint> {
-        size_t operator()(const VertexConstraint& s) const {
+    struct hash<VertexConstraint>
+    {
+        size_t operator()(const VertexConstraint& s) const
+        {
             size_t seed = 0;
             boost::hash_combine(seed, s.time);
             boost::hash_combine(seed, s.x);
@@ -124,7 +131,8 @@ namespace std {
     };
 }  // namespace std
 
-class EdgeConstraint {
+class EdgeConstraint
+{
 public:
     int time;
     int x1;
@@ -136,38 +144,46 @@ public:
     EdgeConstraint(int time, int x1, int y1, int x2, int y2)
             : time(time), x1(x1), y1(y1), x2(x2), y2(y2) {}
 
-    bool operator<(const EdgeConstraint& other) const {
+    bool operator<(const EdgeConstraint& other) const
+    {
         return std::tie(time, x1, y1, x2, y2) <
                std::tie(other.time, other.x1, other.y1, other.x2, other.y2);
     }
 
-    bool operator==(const EdgeConstraint& other) const {
+    bool operator==(const EdgeConstraint& other) const
+    {
         return std::tie(time, x1, y1, x2, y2) ==
                std::tie(other.time, other.x1, other.y1, other.x2, other.y2);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const EdgeConstraint& c) {
+    friend std::ostream& operator<<(std::ostream& os, const EdgeConstraint& c)
+    {
         return os << "EC(" << c.time << "," << c.x1 << "," << c.y1 << "," << c.x2
                   << "," << c.y2 << ")";
     }
 };
 
-namespace std {
+namespace std
+{
     template <>
-    struct hash<EdgeConstraint> {
-        size_t operator()(const EdgeConstraint& s) const {
+    struct hash<EdgeConstraint>
+    {
+        size_t operator()(const EdgeConstraint& s) const
+        {
             size_t seed = 0;
             boost::hash_combine(seed, s.time);
             boost::hash_combine(seed, s.x1);
             boost::hash_combine(seed, s.y1);
             boost::hash_combine(seed, s.x2);
             boost::hash_combine(seed, s.y2);
+
             return seed;
         }
     };
 }  // namespace std
 
-struct Constraints {
+struct Constraints
+{
     std::unordered_set<VertexConstraint> vertexConstraints;
     std::unordered_set<EdgeConstraint> edgeConstraints;
 
