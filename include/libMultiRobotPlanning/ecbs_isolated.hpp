b6f3f474ab84543ffa7e34dbf8f01465d2b20e96
YE GAO <sortsimilar@gmail.com>
2023-12-25 19:35:25 +0800

    306	Move location_valid in LowLevel.

--- include/libMultiRobotPlanning/ecbs_isolated.hpp
+++ include/libMultiRobotPlanning/ecbs_isolated.hpp
@@ -713,31 +713,31 @@ public:
         neighbors.clear();
 
         TimeLocation wait_n(s.time_step + 1, Location(s.location.x, s.location.y));
-        if (m_env.location_valid(wait_n) && m_env.transition_valid(s, wait_n))
+        if (location_valid(wait_n) && m_env.transition_valid(s, wait_n))
         {
             neighbors.emplace_back(Neighbor(wait_n, Action::Wait, 1));
         }
 
         TimeLocation west_n(s.time_step + 1, Location(s.location.x - 1, s.location.y));
-        if (m_env.location_valid(west_n) && m_env.transition_valid(s, west_n))
+        if (location_valid(west_n) && m_env.transition_valid(s, west_n))
         {
             neighbors.emplace_back(Neighbor(west_n, Action::Left, 1));
         }
 
         TimeLocation east_n(s.time_step + 1, Location(s.location.x + 1, s.location.y));
-        if (m_env.location_valid(east_n) && m_env.transition_valid(s, east_n))
+        if (location_valid(east_n) && m_env.transition_valid(s, east_n))
         {
             neighbors.emplace_back(Neighbor(east_n, Action::Right, 1));
         }
 
         TimeLocation north_n(s.time_step + 1, Location(s.location.x, s.location.y + 1));
-        if (m_env.location_valid(north_n) && m_env.transition_valid(s, north_n))
+        if (location_valid(north_n) && m_env.transition_valid(s, north_n))
         {
             neighbors.emplace_back(Neighbor(north_n, Action::Up, 1));
         }
 
         TimeLocation south_n(s.time_step + 1, Location(s.location.x, s.location.y - 1));
-        if (m_env.location_valid(south_n) && m_env.transition_valid(s, south_n))
+        if (location_valid(south_n) && m_env.transition_valid(s, south_n))
         {
             neighbors.emplace_back(Neighbor(south_n, Action::Down, 1));
         }
