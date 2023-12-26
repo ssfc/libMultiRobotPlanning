494f5dcadf7023323d276c232c535ac0abf56a16
YE GAO <sortsimilar@gmail.com>
2023-12-26 17:47:12 +0800

    321	Add std::unordered_set<Location> obstacles to member var.

--- include/libMultiRobotPlanning/ecbs_isolated.hpp
+++ include/libMultiRobotPlanning/ecbs_isolated.hpp
@@ -688,7 +688,7 @@ public:
         const auto& con = m_constraints->vertexConstraints;
         return s.location.x >= 0 && s.location.x < num_columns
             && s.location.y >= 0 && s.location.y < num_rows
-            && m_env.obstacles.find(Location(s.location.x, s.location.y)) == m_env.obstacles.end()
+            && obstacles.find(Location(s.location.x, s.location.y)) == obstacles.end()
             && con.find(VertexConstraint(s.time_step, s.location.x, s.location.y)) == con.end();
     }
 
