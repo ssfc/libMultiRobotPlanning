162f404942142516dbaf3534a3410b08e1e4c9aa
SSFC <sortsimilar@gmail.com>
2024-02-19 19:07:24 +0800

    306	Move struct Conflict.

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -50,6 +50,35 @@ struct hash<State> {
 }  // namespace std
 
 
+struct Conflict {
+    enum Type {
+        Vertex,
+        Edge,
+    };
+
+    int time;
+    size_t agent1;
+    size_t agent2;
+    Type type;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    friend std::ostream& operator<<(std::ostream& os, const Conflict& c) {
+        switch (c.type) {
+            case Vertex:
+                return os << c.time << ": Vertex(" << c.x1 << "," << c.y1 << ")";
+            case Edge:
+                return os << c.time << ": Edge(" << c.x1 << "," << c.y1 << "," << c.x2
+                          << "," << c.y2 << ")";
+        }
+        return os;
+    }
+};
+
+
 template <typename Conflict,
           typename Constraints, typename Task, typename Environment>
 class CBSTA {
