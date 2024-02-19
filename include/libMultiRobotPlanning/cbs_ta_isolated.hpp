926b32e3efdd79c773b6b0e29b063132334710b4
SSFC <sortsimilar@gmail.com>
2024-02-19 22:06:46 +0800

    325	Move astar content.

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -14,10 +14,25 @@
 #include <boost/program_options.hpp>
 
 #include <yaml-cpp/yaml.h>
+
+#include <boost/heap/fibonacci_heap.hpp>
+#include <boost/heap/d_ary_heap.hpp>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "neighbor.hpp"
+#include "planresult.hpp"
+#include "util.hpp"
+
 #include "next_best_assignment.hpp"
 #include "timer.hpp"
-#include "a_star.hpp"
 #include "shortest_path_heuristic.hpp"
+
+using libMultiRobotPlanning::Neighbor;
+using libMultiRobotPlanning::PlanResult;
+
 
 using libMultiRobotPlanning::NextBestAssignment;
 
@@ -622,6 +637,184 @@ public:
 };
 
 
+template <typename Location, typename Action, typename Environment,
+          typename LocationHasher = std::hash<Location> >
+class AStar
+{
+   private:
+    // inner class declaration.
+    class AStarNode;
+
+    // member vars
+    Environment& environment; // include map size, obstacle position, agent goal.
+    // 定义openSet_t和fibHeapHandle_t
+    using OpenSet = boost::heap::fibonacci_heap<AStarNode>;
+    using HeapHandle = typename OpenSet::handle_type;
+    // using OpenSet = boost::heap::d_ary_heap<AStarNode, boost::heap::arity<2>, boost::heap::mutable_<true>>;
+    // using HeapHandle = typename OpenSet::handle_type;
+
+   public:
+    // member funcs
+    AStar(Environment& input_environment) : environment(input_environment) {}
+
+    bool a_star_search(const Location& start_location, PlanResult<Location, Action, int>& solution,
+                       int initialCost = 0)
+    {
+        solution.path.clear();
+        solution.path.emplace_back(std::make_pair<>(start_location, 0));
+        solution.actions.clear();
+        solution.cost = 0;
+
+        OpenSet open_set;
+        std::unordered_map<Location, HeapHandle, LocationHasher> location_to_heap;
+        std::unordered_set<Location, LocationHasher> closed_set;
+        std::unordered_map<Location, std::tuple<Location,Action,int,int>,LocationHasher> came_from;
+
+        auto handle = open_set.push(AStarNode(start_location,
+                                              environment.admissible_heuristic(start_location), initialCost));
+        location_to_heap.insert(std::make_pair<>(start_location, handle));
+        (*handle).handle = handle;
+
+        std::vector<Neighbor<Location, Action, int> > neighbors;
+        neighbors.reserve(10);
+
+        while (!open_set.empty())
+        {
+            AStarNode current = open_set.top();
+            environment.onExpandNode(current.location, current.f_score, current.g_score);
+
+            if (environment.is_solution(current.location))
+            {
+                solution.path.clear();
+                solution.actions.clear();
+                auto iter = came_from.find(current.location);
+                while (iter != came_from.end())
+                {
+                    solution.path.emplace_back(
+                        std::make_pair<>(iter->first, std::get<3>(iter->second)));
+                    solution.actions.emplace_back(std::make_pair<>(
+                        std::get<1>(iter->second), std::get<2>(iter->second)));
+                    iter = came_from.find(std::get<0>(iter->second));
+                }
+
+                solution.path.emplace_back(std::make_pair<>
+                                           (start_location, initialCost));
+                std::reverse(solution.path.begin(), solution.path.end());
+                std::reverse(solution.actions.begin(), solution.actions.end());
+                solution.cost = current.g_score;
+                solution.fmin = current.f_score;
+
+                return true;
+            }
+
+            open_set.pop();
+            location_to_heap.erase(current.location);
+            closed_set.insert(current.location);
+
+            // traverse neighbors
+            neighbors.clear();
+            environment.get_neighbors(current.location, neighbors);
+            for (const Neighbor<Location, Action, int>& neighbor : neighbors)
+            {
+                if (closed_set.find(neighbor.location) == closed_set.end())
+                {
+                    int tentative_gScore = current.g_score + neighbor.cost;
+                    auto iter = location_to_heap.find(neighbor.location);
+                    if (iter == location_to_heap.end())
+                    {  // Discover a new node
+                        int f_score = tentative_gScore + environment.admissible_heuristic(neighbor.location);
+                        auto handle = open_set.push(AStarNode(neighbor.location, f_score, tentative_gScore));
+                        (*handle).handle = handle;
+                        location_to_heap.insert(std::make_pair<>(neighbor.location, handle));
+                        environment.onDiscover(neighbor.location, f_score, tentative_gScore);
+                        // std::cout << "  this is a new node " << f_score << "," <<
+                        // tentative_gScore << std::endl;
+                    }
+                    else
+                    {
+                        auto handle = iter->second;
+                        // std::cout << "  this is an old node: " << tentative_gScore << ","
+                        // << (*handle).g_score << std::endl;
+                        // We found this node before with a better path
+                        if (tentative_gScore >= (*handle).g_score)
+                        {
+                            continue;
+                        }
+
+                        // update f and g_score
+                        int delta = (*handle).g_score - tentative_gScore;
+                        (*handle).g_score = tentative_gScore;
+                        (*handle).f_score -= delta;
+                        open_set.increase(handle);
+                        environment.onDiscover(neighbor.location, (*handle).f_score,
+                                               (*handle).g_score);
+                    }
+
+                    // Best path for this node so far
+                    // TODO: this is not the best way to update "came_from", but otherwise
+                    // default c'tors of Location and Action are required
+                    came_from.erase(neighbor.location);
+                    came_from.insert(std::make_pair<>(neighbor.location,
+                                                      std::make_tuple<>(current.location, neighbor.action, neighbor.cost,
+                                                                        tentative_gScore)));
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+// inner class definition
+template <typename Location, typename Action, typename Environment,
+          typename StateHasher>
+class AStar<Location, Action, Environment, StateHasher>::AStarNode
+{
+   public:
+    Location location;
+    int f_score;
+    int g_score;
+
+    // 定义 handle: 就是上面那个HeapHandle
+    typename boost::heap::fibonacci_heap<AStarNode>::handle_type handle;
+    // typename boost::heap::d_ary_heap<AStarNode, boost::heap::arity<2>, boost::heap::mutable_<true>>::handle_type handle;
+
+   public:
+    AStarNode(const Location& input_state, int input_fScore, int input_gScore)
+        : location(input_state),
+          f_score(input_fScore),
+          g_score(input_gScore)
+    {}
+
+    bool operator<(const AStarNode& other) const
+    {
+        // Sort order
+        // 1. lowest f_score
+        // 2. highest g_score
+
+        // Our heap is a maximum heap, so we invert the comperator function here
+        if (f_score != other.f_score)
+        {
+            return f_score > other.f_score;
+        }
+        else
+        {
+            return g_score < other.g_score;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const AStarNode& node)
+    {
+        os << "location: " << node.location << " f_score: " << node.f_score
+           << " g_score: " << node.g_score;
+
+        return os;
+    }
+
+};
+
+
+
 class CBSTA
 {
 private:
@@ -658,7 +851,7 @@ public:
                 LowLevelSearch_t lowLevel(llenv);
                 success = lowLevel.a_star_search(initialStates[i], start.solution[i]);
             }
-            
+
             if (!success)
             {
                 return false;
