46434b6cf265d81c5a4801213442616da80f5db9
SSFC <sortsimilar@gmail.com>
2024-02-20 07:34:09 +0800

    337	Replace struct with class.

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -192,15 +192,18 @@ namespace std
     };
 }  // namespace std
 
-struct EdgeConstraint
+class EdgeConstraint
 {
-    EdgeConstraint(int time, int x1, int y1, int x2, int y2)
-        : time(time), x1(x1), y1(y1), x2(x2), y2(y2) {}
+public:
     int time;
     int x1;
     int y1;
     int x2;
     int y2;
+
+public:
+    EdgeConstraint(int time, int x1, int y1, int x2, int y2)
+        : time(time), x1(x1), y1(y1), x2(x2), y2(y2) {}
 
     bool operator<(const EdgeConstraint& other) const {
         return std::tie(time, x1, y1, x2, y2) <
