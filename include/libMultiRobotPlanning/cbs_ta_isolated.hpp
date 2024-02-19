18a6cf9918e744351529627c81ad7a5bf5adbbce
SSFC <sortsimilar@gmail.com>
2024-02-20 07:24:19 +0800

    format

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -29,7 +29,8 @@
 using libMultiRobotPlanning::NextBestAssignment;
 
 
-enum class Action {
+enum class Action
+{
     Up,
     Down,
     Left,
@@ -37,36 +38,47 @@ enum class Action {
     Wait,
 };
 
-struct State {
-    State(int time, int x, int y) : time(time), x(x), y(y) {}
 
-    bool operator==(const State& s) const {
-        return time == s.time && x == s.x && y == s.y;
-    }
-
-    bool equalExceptTime(const State& s) const { return x == s.x && y == s.y; }
-
-    friend std::ostream& operator<<(std::ostream& os, const State& s) {
-        return os << s.time << ": (" << s.x << "," << s.y << ")";
-        // return os << "(" << s.x << "," << s.y << ")";
-    }
-
+struct State
+{
     int time;
     int x;
     int y;
-};
 
-namespace std {
-template <>
-struct hash<State> {
-    size_t operator()(const State& s) const {
-        size_t seed = 0;
-        boost::hash_combine(seed, s.time);
-        boost::hash_combine(seed, s.x);
-        boost::hash_combine(seed, s.y);
-        return seed;
+    State(int time, int x, int y) : time(time), x(x), y(y) {}
+
+    bool operator==(const State& s) const
+    {
+        return time == s.time && x == s.x && y == s.y;
+    }
+
+    bool equalExceptTime(const State& s) const
+    {
+        return x == s.x && y == s.y;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const State& s)
+    {
+        return os << s.time << ": (" << s.x << "," << s.y << ")";
+        // return os << "(" << s.x << "," << s.y << ")";
     }
 };
+
+namespace std
+{
+    template <>
+    struct hash<State>
+    {
+        size_t operator()(const State& s) const
+        {
+            size_t seed = 0;
+            boost::hash_combine(seed, s.time);
+            boost::hash_combine(seed, s.x);
+            boost::hash_combine(seed, s.y);
+
+            return seed;
+        }
+    };
 }  // namespace std
 
 
