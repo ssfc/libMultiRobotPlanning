3e82d57ac545f5c6672424d51453698ac5018b3b
SSFC <sortsimilar@gmail.com>
2024-02-20 19:20:13 +0800

    format

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -222,30 +222,36 @@ public:
                std::tie(other.time, other.x1, other.y1, other.x2, other.y2);
     }
 
-    bool operator==(const EdgeConstraint& other) const {
+    bool operator==(const EdgeConstraint& other) const
+    {
         return std::tie(time, x1, y1, x2, y2) ==
                std::tie(other.time, other.x1, other.y1, other.x2, other.y2);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const EdgeConstraint& c) {
+    friend std::ostream& operator<<(std::ostream& os, const EdgeConstraint& c)
+    {
         return os << "EC(" << c.time << "," << c.x1 << "," << c.y1 << "," << c.x2
                   << "," << c.y2 << ")";
     }
 };
 
-namespace std {
-template <>
-struct hash<EdgeConstraint> {
-    size_t operator()(const EdgeConstraint& s) const {
-        size_t seed = 0;
-        boost::hash_combine(seed, s.time);
-        boost::hash_combine(seed, s.x1);
-        boost::hash_combine(seed, s.y1);
-        boost::hash_combine(seed, s.x2);
-        boost::hash_combine(seed, s.y2);
-        return seed;
-    }
-};
+namespace std
+{
+    template <>
+    struct hash<EdgeConstraint>
+    {
+        size_t operator()(const EdgeConstraint& s) const
+        {
+            size_t seed = 0;
+            boost::hash_combine(seed, s.time);
+            boost::hash_combine(seed, s.x1);
+            boost::hash_combine(seed, s.y1);
+            boost::hash_combine(seed, s.x2);
+            boost::hash_combine(seed, s.y2);
+
+            return seed;
+        }
+    };
 }  // namespace std
 
 class Constraints
