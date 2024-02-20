8159499b06d4fe3689f6df472968d290d398cfdc
SSFC <sortsimilar@gmail.com>
2024-02-20 16:40:46 +0800

    change back

--- include/libMultiRobotPlanning/cbs_ta_isolated.hpp
+++ include/libMultiRobotPlanning/cbs_ta_isolated.hpp
@@ -764,7 +764,6 @@ class AStar
 private:
     // member vars
     LowLevelEnvironment& low_level_environment; // include map size, obstacle position, agent goal.
-    Environment& m_env;
 
     // 定义openSet_t和fibHeapHandle_t
     using OpenSet = boost::heap::fibonacci_heap<AStarNode>;
@@ -775,8 +774,7 @@ private:
 public:
     // member funcs
     AStar(LowLevelEnvironment& input_environment, Environment& environment)
-     : low_level_environment(input_environment),
-       m_env(environment)
+     : low_level_environment(input_environment)
     {}
 
     // CBSTA(Environment& environment) : m_env(environment) {}
@@ -881,150 +879,6 @@ public:
                                                       std::make_tuple<>(current.location, neighbor.action, neighbor.cost,
                                                                         tentative_gScore)));
                 }
-            }
-        }
-
-        return false;
-    }
-
-    bool cbsta_search(const std::vector<State>& initialStates, std::vector<PlanResult>& solution)
-    {
-        HighLevelNode start;
-        size_t numAgents = initialStates.size();
-        start.solution.resize(numAgents);
-        start.constraints.resize(numAgents);
-        start.cost = 0;
-        start.id = 0;
-        start.isRoot = true;
-        m_env.nextTaskAssignment(start.tasks);
-
-        for (size_t i = 0; i < initialStates.size(); ++i)
-        {
-            // if (   i < solution.size()
-            //     && solution[i].path.size() > 1) {
-            //   start.solution[i] = solution[i];
-            //   std::cout << "use existing solution for agent: " << i << std::endl;
-            // } else {
-            bool success = false;
-            if (!start.tasks.empty())
-            {
-                LowLevelEnvironment llenv(m_env, i, start.constraints[i],
-                                          start.task(i));
-                AStar lowLevel(llenv, m_env);
-                success = lowLevel.a_star_search(initialStates[i], start.solution[i]);
-            }
-
-            if (!success)
-            {
-                return false;
-            }
-            // }
-            start.cost += start.solution[i].cost;
-        }
-
-        // std::priority_queue<HighLevelNode> open;
-        typename boost::heap::d_ary_heap<HighLevelNode, boost::heap::arity<2>,
-                                         boost::heap::mutable_<true> > open;
-
-        auto handle = open.push(start);
-        (*handle).handle = handle;
-
-        solution.clear();
-        int id = 1;
-        while (!open.empty())
-        {
-            HighLevelNode P = open.top();
-            m_env.onExpandHighLevelNode(P.cost);
-            // std::cout << "expand: " << P << std::endl;
-
-            open.pop();
-
-            Conflict conflict;
-            if (!m_env.getFirstConflict(P.solution, conflict))
-            {
-                std::cout << "done; cost: " << P.cost << std::endl;
-                solution = P.solution;
-
-                return true;
-            }
-
-            if (P.isRoot)
-            {
-                // std::cout << "root node expanded; add new root" << std::endl;
-                HighLevelNode n;
-                m_env.nextTaskAssignment(n.tasks);
-
-                if (n.tasks.size() > 0)
-                {
-                    n.solution.resize(numAgents);
-                    n.constraints.resize(numAgents);
-                    n.cost = 0;
-                    n.id = id;
-                    n.isRoot = true;
-
-                    bool allSuccessful = true;
-                    for (size_t i = 0; i < numAgents; ++i)
-                    {
-                        LowLevelEnvironment llenv(m_env, i, n.constraints[i], n.task(i));
-                        AStar lowLevel(llenv, m_env);
-                        bool success = lowLevel.a_star_search(initialStates[i], n.solution[i]);
-                        if (!success)
-                        {
-                            allSuccessful = false;
-                            break;
-                        }
-
-                        n.cost += n.solution[i].cost;
-                    }
-
-                    if (allSuccessful)
-                    {
-                        auto handle = open.push(n);
-                        (*handle).handle = handle;
-                        ++id;
-                        std::cout << " new root added! cost: " << n.cost << std::endl;
-                    }
-                }
-            }
-
-            // create additional nodes to resolve conflict
-            // std::cout << "Found conflict: " << conflict << std::endl;
-            // std::cout << "Found conflict at t=" << conflict.time << " type: " <<
-            // conflict.type << std::endl;
-
-            std::map<size_t, Constraints> constraints;
-            m_env.createConstraintsFromConflict(conflict, constraints);
-            for (const auto& c : constraints)
-            {
-                // std::cout << "Add HL node for " << c.first << std::endl;
-                size_t i = c.first;
-                // std::cout << "create child with id " << id << std::endl;
-                HighLevelNode newNode = P;
-                newNode.id = id;
-                // (optional) check that this constraint was not included already
-                // std::cout << newNode.constraints[i] << std::endl;
-                // std::cout << c.second << std::endl;
-                assert(!newNode.constraints[i].overlap(c.second));
-
-                newNode.constraints[i].add(c.second);
-
-                newNode.cost -= newNode.solution[i].cost;
-
-                LowLevelEnvironment llenv(m_env, i, newNode.constraints[i],
-                                          newNode.task(i));
-                AStar lowLevel(llenv, m_env);
-                bool success = lowLevel.a_star_search(initialStates[i], newNode.solution[i]);
-
-                newNode.cost += newNode.solution[i].cost;
-
-                if (success)
-                {
-                    // std::cout << "  success. cost: " << newNode.cost << std::endl;
-                    auto handle = open.push(newNode);
-                    (*handle).handle = handle;
-                }
-
-                ++id;
             }
         }
 
